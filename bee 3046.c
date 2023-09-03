eaf18f11d5882f215c571bfa11cc67a8c96b935d
Juan perri <111884460+solarisjuan@users.noreply.github.com>
2023-09-02 21:08:33 -0300

    Add files via upload

---
+++ bee 3046.c
@@ -0,0 +1,13 @@
+//bee 3046
+
+#include<stdio.h>   
+
+    int main(){
+        int entrada,aux;
+        scanf("%d", &entrada);
+ 
+           aux=((entrada+1)*(entrada+2)/2);
+        
+        printf("%d", aux);
+        return 0;
+    }
